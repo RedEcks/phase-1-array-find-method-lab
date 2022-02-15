9a9b8a0d633d3ff253154f027f563e6201c25283
Xavier Dunkley <xavierxv18@gmail.com>
2022-02-15 15:00:30 -0500

    commit tree bf4c6cdde43813dd60944719bd36fe255519fe4e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array){
+    const found = array.find(object => object.result==='W')
+    console.log(found)
+    if (found){
+        return found.year
+    }
+}
+
